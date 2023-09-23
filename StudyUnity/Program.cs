84262195042ab9956b25e41cd430bd76266f6393
Snow0406 <han060406@gmail.com>
2023-09-24 03:42:32 +0900

    C# study 02
    day 03

--- StudyUnity/Program.cs
+++ StudyUnity/Program.cs
@@ -2,24 +2,29 @@
 
 namespace StudyUnity
 {
+    public static class ExtensionMethodClass
+    {
+        public static string toSnow(this object input)
+        {
+            return "스노우 히히";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] num = { 1,2,3,4,5,6,7,8,9 };
+            int num1 = 10;
+            int num2 = 20;
+            object num3 = 30;
 
-            foreach (int number in num)
-            {
-                Console.WriteLine($"3 * {number} = {number * 3}");
-            }
+            Set(num1, num2);
+            Console.WriteLine(num3.toSnow());
+        }
 
-
-            Console.WriteLine("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-
-            for (int i = 1; i < num.Length+1; i++)
-            {
-                Console.WriteLine($"3 * {i} = {i * 3}");
-            }
+        static void Set(int a = 1, int b = 2)
+        {
+            Console.WriteLine(a + b);
         }
     }
 }
