ecd979e7c94dc70e61f1378107ca54bb0094314b
Snow0406 <han060406@gmail.com>
2023-10-13 00:08:37 +0900

    Update

--- StudyUnreal/cMain.cpp
+++ StudyUnreal/cMain.cpp
@@ -1,57 +1,12 @@
 #include <iostream>
-
-struct Pet
-{
-	const char* name;
-
-	Pet(const char* _name)  //생성되었을때 호출되는 함수
-	{
-		name = _name;
-		std::cout << name << "생성" << std::endl;
-	}
-
-	~Pet() //소멸되었을때 호출되는 함수
-	{
-		std::cout << name << "소멸" << std::endl;
-	}
-};
-
-struct Player
-{
-	const char* name;
-	Pet* pet;
-
-	Player(const char* _name)  //생성되었을때 호출되는 함수
-	{
-		name = _name;
-		pet = nullptr;
-		std::cout << name << "생성" << std::endl;
-	}
-
-	~Player() //소멸되었을때 호출되는 함수
-	{
-		if (pet != nullptr)
-		{
-			delete pet;
-			pet = nullptr;
-		}
-		std::cout << name << "소멸" << std::endl;
-	}
-};
-
+#include "Player.h"
 
 int main() {
-	Player* player = new Player("glgl");
-	Pet* pet = new Pet("댕댕이");
+	Player id1;
+	
+	id1.money = 5000;
 
-	player->pet = pet;
-	player->pet->name = "멍멍이";
-
-	std::cout << pet->name << std::endl;
-
-	delete player;
-	player = nullptr;
-
+	std::cout << id1.GetUserID() << "님의 돈 : " << id1.money << std::endl;
 
 	return 0;
 }
