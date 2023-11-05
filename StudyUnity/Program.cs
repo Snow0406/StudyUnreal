3639b26d41869cac23f2fddf9407c1a4cecdd612
Snow0406 <han060406@gmail.com>
2023-11-05 12:30:38 +0900

    C# study - Day 18
    linq - from, where,m orderby, select

--- StudyUnity/Program.cs
+++ StudyUnity/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace Snow
 {
     class Program
     {
-        class Snow {
-            float playerX = 10;
-            float playerY = 20;
-            public float Distance { get => MathF.Abs(playerX - playerY); }
-        }
-
-        public delegate void Yuki();
-        static Yuki yuki;
         static void Main()
         {
-            //yuki = delegate () { Console.WriteLine("asdf"); };
-            //yuki();
-            //
-            //yuki = () => { Console.WriteLine("Lamda Yuki"); };
-            //yuki();
+            List<int> list = new List<int>();
+            while (true) {
+                string? s = Console.ReadLine();
+                if (s == "0") {
+                    var linqData = from data in list where data % 3 == 0 orderby data ascending select data;
+                    Console.WriteLine("=========================");
+                    foreach (var data in linqData) {
+                        Console.WriteLine(data);
+                    }
+                    break;
+                } else {
+                    list.Add(int.Parse(s));
+                }
+            }
 
-            Snow snow = new Snow();
-            Console.WriteLine(snow.Distance); 
         }
     }
 }
