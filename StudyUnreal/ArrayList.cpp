3e7c490edd7ae703f3b0d24640009f6b6c90ca46
snow <han060406@gmail.com>
2023-11-15 01:55:49 +0900

    day05
    SingleList

--- StudyUnreal/ArrayList.cpp
+++ StudyUnreal/ArrayList.cpp
@@ -2,52 +2,164 @@
 
 void SingleList::AddNode(int _data)
 {
-	if (IsEmpty()) { //count 0
-		Node node;
-		node.data = _data; //10
-		node.next = NULL;
-		head = &node;
+	Node* node = new Node();
+	count++;
+
+	node->data = _data;
+	node->next = NULL;
+
+	if (head == NULL) {
+		head = node;
 	}
 	else {
 		Node* temp = head;
-		//if (temp->next != NULL) { //count 2 이상
-		//	Node* node = new Node();
-		//
-		//	node->data = _data;
-		//	node->next = NULL;
-		//	temp = temp->next;
-		//}
-		else { //count 1
-			Node node1;
-			node1.data = _data; //2
-			node1.next = NULL;
-			temp->next = &node1;
+		while (true)
+		{
+			if (temp->next != NULL) temp = temp->next;
+			else {
+				temp->next = node;
+				//delete temp;
+				break;
+			}
 		}
 	}
 }
 void SingleList::InsertNode(int _index, int _data)
 {
+	if (IsEmpty() || count+1 < _index) {
+		AddNode(_data);
+		return;
+	}
+
+	Node* node = new Node();
+	node->data = _data;
+	node->next = NULL;
+
+	if (_index == 0) { //0번째일때
+		node->next = head;
+		head = node;
+	}
+
+	Node* tempNode = head;
+	int index = 0;
+
+	while (true)
+	{
+		if (index == _index - 1) { //frontNode
+			node->next = tempNode->next;
+			tempNode->next = node;
+			break;
+		}
+		index++;
+		tempNode = tempNode->next;
+	}
+
+	count++;
 }
 
 void SingleList::UpdateNode(int _index, int _data)
 {
+	if (IsEmpty() || count < _index) return;
+
+	Node* temp = head;
+	for (int i = 0; i < count; i++)
+	{
+		if (i == _index) {
+			temp->data = _data;
+			break;
+		}
+		temp = temp->next;
+	}
 }
 
 void SingleList::DeleteNodeData(int _data)
 {
+	if (IsEmpty()) return;
+
+	Node* temp = head;
+	Node* past = head;
+	while (past != NULL)
+	{
+		if (temp->data == _data) {
+			past->next = temp->next;
+			break;
+		}
+		else {
+			past = temp;
+			temp = temp->next;
+		}
+	}
+
+	if (past == NULL) return
+	delete temp;
+	count--;
 }
 
 void SingleList::DeleteIndex(int _index)
 {
+	if (IsEmpty() || count < _index) return;
+
+	Node* temp = head;
+	Node* past = head;
+	int index = 0;
+
+	while (past != NULL)
+	{
+		if (index == _index) {
+			past->next = temp->next;
+			break;
+		}
+		else {
+			past = temp;
+			temp = temp->next;
+			index++;
+		}
+	}
+	delete temp;
+	count--;
 }
 
 void SingleList::ClearAllNode()
 {
+	Node* temp = head;
+	Node* past = head;
+
+	while (true)
+	{
+		if (temp->next != NULL) {
+			past = temp;
+			temp = temp->next;
+			delete past;
+		}
+		else {
+			break;
+		}
+	}
+
+	head = NULL;
+	count = 0;
 }
 
 int SingleList::GetNodeData(int _index)
 {
-	return 0;
+	if (IsEmpty() || count < _index) return -1;
+
+	Node* temp = head;
+	int index = 0;
+	int res = 0;
+
+	while (true)
+	{
+		if (index == _index) {
+			res = temp->data;
+			break;
+		}
+		else {
+			temp = temp->next;
+			index++;
+		}
+	}
+	return res;
 }
 
 int SingleList::GetListSize()
@@ -63,6 +175,12 @@ bool SingleList::IsEmpty()
 
 void SingleList::PrintAll()
 {
+	Node* temp = head;
+	for (int i = 0; i < count; i++)
+	{
+		cout << temp->data << endl;
+		temp = temp->next;
+	}
 }
 
 SingleList::SingleList()
--- StudyUnreal/cMain.cpp
+++ StudyUnreal/cMain.cpp
@@ -1,15 +1,33 @@
 #include "ArrayList.h"
 
 int main()
-{ 
+{
 	SingleList singleList;
 
 	singleList.AddNode(10);
 	singleList.AddNode(20);
 	singleList.AddNode(30);
+	singleList.AddNode(40);
+	singleList.AddNode(50);
 
-	cout << singleList.head->data << endl;
-	//cout << singleList.head->next->data << endl;
+	singleList.InsertNode(2, 77);
+
+	singleList.UpdateNode(3, 33);
+	singleList.UpdateNode(4, 44);
+
+	singleList.DeleteNodeData(77);
+	singleList.DeleteIndex(2);
+
+	singleList.PrintAll();
+
+	cout << "================" << endl;
+
+	cout << singleList.GetNodeData(1) << endl;
+	singleList.ClearAllNode();
+
+	cout << "================" << endl;
+
+	cout << singleList.GetListSize() << endl;
 
 	return 0;
 }
