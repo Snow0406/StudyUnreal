f709542347e01e829a1f0c90d9f9180928a9faa9
snow <han060406@gmail.com>
2023-11-15 21:46:28 +0900

    ud

--- StudyUnreal/cMain.cpp
+++ StudyUnreal/cMain.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
 using namespace std;
 
-//#define SWAP(a,b) {int temp; temp = a; a = b; b = temp;}
-#define MAX 10
+#define SWAP(a,b) {int temp; temp = a; a = b; b = temp;}
 
-void BubbleSort(int* pNum, int num);
+void SelectionSort(int* pNum, int num);
 void Show(int* pArray, int num);
 
 void main()
 {
-	int array[MAX] = { 3,0,1,8,7,2,5,4,6,9 };
-	BubbleSort(array, MAX);
-	BubbleSort(array, MAX);
+	int array[10] = { 3,0,1,8,7,2,5,4,9,6 };
+	SelectionSort(array, 10);
 }
 
-void BubbleSort(int* pArray, int num)
+void SelectionSort(int* pArray, int num)
 {
 	Show(pArray, num);
-
-	int isEnd = 0;
-	while (true)
+	
+	int newArray[10] = {};
+	for (int i = 0; i < 10; i++)
 	{
-		for (int i = 0; i < MAX - 1; i++)
-		{
-			int num;
-			if (pArray[i] < pArray[i + 1]) { //ex: 2 4
-				isEnd++;
+			int mainNum = pArray[i]; //3
+			for (int j = i + 1; j < 9 - i; j++) //1 //2 //3
+			{
+				if (mainNum < pArray[j]) { //3 0 //0 1 //
+					continue;
+				} else if (mainNum > pArray[j]) { //3 0 //0 1
+					mainNum = pArray[j]; //0 //0
+				}
 			}
-			else if (pArray[i] > pArray[i + 1]) { //ex: 4 2
-				num = pArray[i];
-				pArray[i] = pArray[i + 1];
-				pArray[i + 1] = num;
-			} else { //ex: 2 2
-				isEnd++;
-			}
-		}
-
-		if (isEnd == MAX - 1) {
-			Show(pArray, num);
-			break;
-		} else isEnd = 0;
+			newArray[i] = pArray[i];
 	}
-}
+	pArray = newArray;
+	  Show(pArray, num);
+	}
 
 void Show(int* pArray, int num)
 {
