5854266c5626292d751066ce0b6ecff4de8fa0ea
snow <han060406@gmail.com>
2023-11-16 21:19:14 +0900

    삽입정렬 Insert Sort

--- StudyUnreal/cMain.cpp
+++ StudyUnreal/cMain.cpp
@@ -3,36 +3,30 @@ using namespace std;
 
 #define SWAP(a,b) {int temp; temp = a; a = b; b = temp;}
 
-void SelectionSort(int* pNum, int num);
+void InsertSort(int* pNum, int num);
 void Show(int* pArray, int num);
 
 void main()
 {
 	int array[10] = { 3,0,1,8,7,2,5,4,9,6 };
-	SelectionSort(array, 10);
+	InsertSort(array, 10);
 }
 
-void SelectionSort(int* pArray, int num)
+void InsertSort(int* pArray, int num)
 {
 	Show(pArray, num);
-	
-	int newArray[10] = {};
 	for (int i = 0; i < 10; i++)
 	{
-			int mainNum = pArray[i]; //3
-			for (int j = i + 1; j < 10; j++) //1 //2 //3
-			{
-				if (mainNum < pArray[j]) { //3 0 //0 1 //
-					continue;
-				} else if (mainNum > pArray[j]) { //3 0 //0 1
-					SWAP(mainNum, pArray[j]); //0 //0
-				}
+		for (int j = i-1; j >= 0; j--)
+		{
+			if (pArray[j] > pArray[j + 1]) {
+				SWAP(pArray[j], pArray[j + 1]);
+				Show(pArray, num);
 			}
-			newArray[i] = mainNum;
+		}
 	}
-	pArray = newArray;
-	  Show(pArray, num);
-	}
+	Show(pArray, num);
+}
 
 void Show(int* pArray, int num)
 {
@@ -42,4 +36,5 @@ void Show(int* pArray, int num)
 	}
 
 	cout << endl;
+	cout << "==================" << endl;
 }
